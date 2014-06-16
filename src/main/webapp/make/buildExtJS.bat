f63eb4ced53d0c49d6adfae4c26e063a9566a95f
cwtuan <cwtuan@cht.com.tw>
2014-06-16 09:56:53 +0800

    timestamp

--- src/main/webapp/make/buildExtJS.bat
+++ src/main/webapp/make/buildExtJS.bat
@@ -1,4 +1,5 @@
-set webapp=D:\workspace\ExtJSTutorial\src\main\webapp
+set ssrPath=%CD%
+set webapp=%CD%\..
 
 
 copy /Y %webapp%\WEB-INF\jsp\loadjs_dev.jspf %webapp%\
@@ -8,11 +9,10 @@ move /Y %webapp%\WEB-INF\jsp\all-classes.js %webapp%\
 del %webapp%\loadjs_dev.jspf
 
 
-set t=%time::=-%
-set t=%t:.=-%
-set timestamp=%Date:~0,4%-%Date:~5,2%-%Date:~8,2%-%t%
+ 
+set timestamp=%Date:~0,4%-%Date:~5,2%-%Date:~8,2%_%time:~-10,1%-%time:~-8,2%-%time:~-5,2%
 
-set ssrPath=D:\workspace\ExtJSTutorial\src\main\webapp\make
+
 cd %ssrPath%
 ssr.exe -f %webapp%\WEB-INF\jsp\loadjs.jspf -a -s all-classes.js -r all-classes.js?timestamp=%timestamp%
 del %webapp%\WEB-INF\jsp\loadjs.jspf.bak*
